7690ad1586b5cf609a0dfe62e5919217af3fc26e
danilo992 <tadeudanilo46@gmail.com>
2020-11-23 12:19:48 -0300

    importando fonte e criando o diseng css3 e criando variaveis com javascript.

--- js/editor.js
+++ js/editor.js
@@ -25,4 +25,61 @@ var CDEditor = function (textarea) {
     this.save = function () {
         textareaSource.value = CDEditorIframe.document.body.innerHTML;
     };
+
+    var Component = function (commandName, element, event) {
+        this.commandName = commandName;
+        this.element = document.createElement('li');
+        this.element.appendChild(element);
+        this.recoverValue = function () {
+            return null;
+        };
+
+        var selfComponent = this;
+        this.element.addEventListener(event, function () {
+            CDEditoIframe.document.execCommand(commandName, false, selfComponent.recoverValue());
+        });
+    };
+
+    var ComponentButton = function (commandName, icon) {
+        var button = document.createElement('button');
+        var buttonIcon = document.createElement('i');
+        buttonIcon.classList.add('fa', 'fa-', + icon);
+        button.appendChild(buttonIcon);
+        Component.call(this,commandName, button, 'click');
+    };
+
+    var ComponentSelect = function (commandName, valeus) {
+        var select = document.createElement('select');
+        valeus.forEach(function (value) {
+            var option = document.createElement('option');
+            option.value = value;
+            option.appendChild(document.createTextNode(value));
+            select.appendChild(option);
+        });
+
+        Component.call(this, commandName, select, 'change');
+
+        var selfComponentSelect = this;
+        this.recoverValue = function () {
+            return selfComponentSelect.element.firsChild.value;
+        };
+    };
+
+    var Space = function () {
+        this.element = document.createElement('li');
+        this.element.classList.add('space');
+        this.element.innerHTML = '&nbsp;';
+    };
+
+    var selectedNode = function () {
+        return CDEditorIframe.getSelection().anchorNode.parentNode; 
+    };
+
+var initToolbar = function (container, toolbar) {
+    //HIGNLIGHTER BUTTON
+    var highlighter = new ComponentButton('backColor', 'highlighter');
+    highlighter.recoverValue = function () {
+        return selectedNode().style.backgroundColor === 'yellow' ? 'white' : 'yellow';
+    };
+    
 };
